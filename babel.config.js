98b1d336b0d09af15dc9fde3647dee1ba81229d3
selezai <selezai@gmail.com>
2025-01-02 23:45:55 +0200

    Fix TypeScript configuration
    - Update tsconfig.json to use correct path for expo base config
    - Add proper path aliases
    - Install required TypeScript type definitions

--- babel.config.js
+++ babel.config.js
@@ -10,6 +10,13 @@ module.exports = function(api) {
         allowUndefined: false,
         envName: process.env.NODE_ENV || 'development'
       }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+      '@babel/plugin-transform-async-generator-functions',
+      '@babel/plugin-transform-object-rest-spread',
+      '@babel/plugin-transform-optional-chaining',
+      '@babel/plugin-transform-nullish-coalescing-operator',
       'react-native-reanimated/plugin'
     ]
   };
